c6a3177278062c1b36efa150bc8fcd86b7975e6f
Ayan Chavand <ayan.chavand15@gmail.com>
2020-11-06 12:23:04 +0530

    new stuff

--- Selenium/idk.py
+++ Selenium/idk.py
@@ -10,25 +10,22 @@ path = "C:\Program Files (x86)\chromedriver.exe"
 driver = webdriver.Chrome(path)
 #get on the website
 driver.get("https://techwithtim.net")
-#what's the title
-print(driver.title)
-#find stuff with the name s
-search = driver.find_element_by_name("s")
-#type test
-search.send_keys("test")
-#hit enter
-search.send_keys(Keys.RETURN)
-
 
 try:
     #wait until the search loades
-    main = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.ID, "main"))
+    element = WebDriverWait(driver, 30).until(
+        EC.presence_of_element_located((By.LINK_TEXT,"Django Tutorial"))
     )
-    #print the text from the main element
-    print(main.text)
+    element.click()
+    element = WebDriverWait(driver, 30).until(
+        EC.presence_of_element_located((By.ID,"sow-button-19310003"))
+    )
+    element.click()
+    
 except:
+    input()
     driver.quit()
 
-driver.quit()
+
+#driver.quit()
 
